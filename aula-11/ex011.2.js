69bf4a8bb6dddc81f4773455dbab47588aabb793
Joao Vitor Simon Moreira <vitor.simonlegit@gmail.com>
2023-11-03 02:18:12 -0300

    .

---
+++ aula-11/ex011.2.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`você nasceu no país ${país}`)
+if(país=='Brasil'){
+  console.log('Você é Brasileiro')
+}else{
+  console.log('você é estrangeiro')
+}
--- aula-11/ex011.js
+++ aula-11/ex011.js
@@ -1,2 +1,10 @@
-var vel = ('12')
+var vel = ('61')
+console.log(`Sua velocidade foi ${vel}km/h`)
+if(vel > 60){
+  console.log(`Sua velocidade ultrapassou o limite, MULTADO!`)
+  
+}
 console.log(`dirija sempre com o sinto de segurança`)
+
+//sinais= ==(igual) >=(maior ou igual) <=(menor ou igual) 
+//!=(diferente)
